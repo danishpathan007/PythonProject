19639099aaa23f78adec39aea8c671e5f5a8ea61
danishpathan007 <danish.lpu.007@gmail.comssh-keygen -t rsa -C danish.lpu.007@gmail.comcat>
2018-08-03 14:22:25 +0530

    Hello

---
+++ helloworld.py
@@ -0,0 +1,13 @@
+
+a = 10
+print("Hello world!",a)
+
+#Single line comments start with '#'
+
+"""
+  Multipleline string can be
+  written using three "s ,  and
+  are often used as function
+"""
+
+'''This is doc_string'''
---
+++ variable.py
@@ -0,0 +1,23 @@
+# Variables
+
+x = 2 #No Semicolon
+y = x * 7
+print(y)
+
+x = "Hello , I'am "
+z= x + "python"
+print(z)
+
+String_way1 = 'hello World'
+print(String_way1)
+String_way2 = "Hello world"
+print(String_way2)
+String_way3 = ''' Hello World  '''
+print(String_way3)
+
+'''
+type(1)
+type('h')
+type("HEllo")
+
+'''
