6fefa5de9445a70c132a84deedfbe61c78d833d6
danishpathan007 <danish.lpu.007@gmail.comssh-keygen -t rsa -C danish.lpu.007@gmail.comcat>
2018-08-03 15:07:21 +0530

    updated

--- variable.py
+++ variable.py
@@ -21,3 +21,29 @@ type('h')
 type("HEllo")
 
 '''
+x = 1
+print(x==1)
+name = 'Danish'+''+'khan'
+print(name)
+
+s = "DANISH"
+print(s[0])
+print(s[1])
+print(s[2])
+print(s[3])
+print(s[4])
+print(s[5])
+#print(s[6]) IndexError: string Index out of range
+
+#Negative Index
+print(s[-1])
+print(s[-2])
+print(s[-3])
+print(s[-4])
+print(s[-5])
+#Slicing
+print(s[0:2])
+print(s[0:4])
+print(s[:4]) #by defaut it's start from index[0]
+print(s[1:]) #its run upto last index
+print(s[1:5:2]) # 1st is starting index , 2nd is last index and the 3rd one is step size and last index is also excluded
