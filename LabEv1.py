561f6dfcebf94427ea28f43ea69a89d9572d0045
Danish Khan <32009304+danishpathan007@users.noreply.github.com>
2018-08-24 14:03:57 +0530

    Lab Ev1

---
+++ LabEv1.py
@@ -0,0 +1,9 @@
+em_name=("Danish Khan")
+em_ID=(11706493)
+phn_number=['8923324006','9690739077']
+em_DOB=(" 8th March 1997")
+em_perm_address=("Town Nanauta ,Dist Saharanpur, state U.P")
+Emp_Details={"Employee_Name":em_name,"Employee_ID":em_ID,"Contacts":phn_number,"DOB":em_DOB,"Salary":25000,"Permanent_Adress":em_perm_address,"Current_Address":"Lovely professional University","Experience":"5 year","Position":"Manager"}
+for k,v in Emp_Details.items():
+	print(k,"=",v)
+
